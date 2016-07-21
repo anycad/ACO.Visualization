a97a72de373a405bb039162085ee94562b52cd60
liwei <liwei.ma@outlook.com>
2016-07-21 15:20:59 +0800

    add selection event handler

--- VB_Net_Sample/Form1.vb
+++ VB_Net_Sample/Form1.vb
@@ -2,9 +2,14 @@
     Private renderView As AnyCAD.Presentation.RenderWindow3d
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        'Initialize the 3D control
         renderView = New AnyCAD.Presentation.RenderWindow3d
         renderView.Size = Panel1.ClientSize
         Panel1.Controls.Add(renderView)
+
+        'Register Selection Event Handler
+        AddHandler AnyCAD.Platform.GlobalInstance.EventListener.OnSelectElementEvent, AddressOf Selection_Changed
     End Sub
 
     Private Sub Form1_SizeChanged(sender As System.Object, e As System.EventArgs) Handles MyBase.SizeChanged
@@ -12,11 +17,17 @@
             renderView.Size = Panel1.ClientSize
         End If
     End Sub
-
+    'Create the shape
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
 
         Dim ball = AnyCAD.Platform.GlobalInstance.BrepTools.MakeSphere(AnyCAD.Platform.Vector3.ZERO, 100)
         renderView.ShowGeometry(ball, 100)
         renderView.RequestDraw()
     End Sub
+
+    ' Selection Event Handler
+    Private Sub Selection_Changed(args As AnyCAD.Platform.SelectionChangeArgs)
+        MsgBox(args.GetIds().ToString())
+    End Sub
+
 End Class
